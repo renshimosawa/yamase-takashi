06cdf83dc412f1cc1397bcc52df148c3541d709f
renshimosawa <polaris2760@gmail.com>
2026-05-04 00:26:56 +0900

    アダプティブに

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -17,13 +17,13 @@ export default function manifest(): MetadataRoute.Manifest {
     categories: ["social", "weather", "lifestyle"],
     icons: [
       {
-        src: "/favicon.png",
+        src: "/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any",
+        purpose: "maskable",
       },
       {
-        src: "/yamasekun_base.png",
+        src: "/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "maskable",
